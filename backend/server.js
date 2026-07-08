3143b87ddc532f06f99567923a2881fd46aa8f51
Riazuddinsk <skriazuddin974@gmail.com>
2026-07-08 16:19:24 +0530

    connect buyer's routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import authRoutes2 from './routes/authRoutes2.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/auth2',authRoutes2);
 
 app.get('/', (req, res )=>{
     res.send ('Api is running..');
