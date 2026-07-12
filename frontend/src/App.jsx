f4cafe7cb2f82f7b188f8cb00e6dab1ee6611a35
Riazuddinsk <skriazuddin974@gmail.com>
2026-07-12 19:07:49 +0530

    Add routes for customerSignup , customerLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter, RouterProvider, Outlet} from "react-router";
 import Signup from "./pages/Signup"
 import Login from "./pages/Login";
+import Signup2 from "./pages/CoustomerSignup";
+import Login2 from "./pages/CustomerLogin";
 
 function Layout() {
   return (
@@ -14,13 +16,10 @@ const router = createBrowserRouter([
   {
     element:<Layout />,
     children:[
-      {
-        path:"/signup",element:<Signup/>
-  
-      },
-      {
-        path:"/login",element:<Login/>
-      }
+      { path:"/signup",element:<Signup/> },
+      { path:"/login",element:<Login/> },
+      { path:"/customerSignup",element:<Signup2/>},
+      { path:"/customerLogin", element:<Login2/>}
     ]
   }
 ])
