c1d317e55021cdf707036c103ef789a14f1afcaf
Riazuddinsk <skriazuddin974@gmail.com>
2026-08-01 09:31:17 +0530

    add routes for add, edit, delete product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login";
 import Signup2 from "./pages/CoustomerSignup";
 import Login2 from "./pages/CustomerLogin";
+import AddProduct from "./admin_panel/AddProduct";
+import EditProduct from "./admin_panel/EditProduct"
+import ProductList from "./admin_panel/ProductList";
 
 function Layout() {
   return (
@@ -19,7 +22,10 @@ const router = createBrowserRouter([
       { path:"/signup",element:<Signup/> },
       { path:"/login",element:<Login/> },
       { path:"/customerSignup",element:<Signup2/>},
-      { path:"/customerLogin", element:<Login2/>}
+      { path:"/customerLogin", element:<Login2/>},
+      { path:"/addProduct", element:<AddProduct/>},
+      { path:"/editProduct/:id", element:<EditProduct/>},
+      { path:"/products", element:<ProductList/>}
     ]
   }
 ])
