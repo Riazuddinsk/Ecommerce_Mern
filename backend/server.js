bc70739cf92b721f47b8ac1462b86d69c4ad21dd
Riazuddinsk <skriazuddin974@gmail.com>
2026-08-01 09:16:24 +0530

    Change server file due to add cart and product path

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import authRoutes2 from './routes/authRoutes2.js';
+import productRouter from "./routes/productRouter.js"
+import cartRouter from "./routes/cartRouter.js"
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/auth2',authRoutes2);
+app.use('/api/products',productRouter);
+app.use('/api/cart',cartRouter)
 
 app.get('/', (req, res )=>{
     res.send ('Api is running..');
